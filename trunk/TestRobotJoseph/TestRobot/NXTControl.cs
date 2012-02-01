ecfcef120a20d23f3e2b55c3ab74401c9ec16835
alx.viola@gmail.com <alx.viola@gmail.com@255d768a-8558-bab0-4c0f-9fcaf0b8a584>
2012-02-01 09:05:09 +0000

    projet joseph test for video

---
+++ trunk/TestRobotJoseph/TestRobot/NXTControl.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NKH.MindSqualls;
+
+namespace TestRobot
+{
+    public class NXTControl
+    {
+        private NxtBrick BlockMoteur;
+        private Nxt2ColorSensor CapteurCouleur;
+        private static bool EndAction;
+        private uint OuverturePince;
+        private bool PlierState;
+        private string ColorChecked;
+
+        public NXTControl ()
+        {
+            //Instanciation du block moteur
+            BlockMoteur = new NxtBrick(NxtCommLinkType.USB, 5);
+            
+            //Moteur Pince
+            BlockMoteur.MotorA = new NxtMotor();
+            //Moteur Gauche
+            BlockMoteur.MotorB = new NxtMotor();
+            //Moteur Droit
+            BlockMoteur.MotorC = new NxtMotor();
+
+            //Capteur de couleur
+            CapteurCouleur = new Nxt2ColorSensor();
+            
+            //Initialisation du capteur
+            BlockMoteur.Sensor2 = CapteurCouleur;
+            CapteurCouleur.SetColorDetectorMode();
+
+            //Degré d'ouverture de la pince
+            OuverturePince = 20;
+            PlierState = false;
+
+            ColorChecked = "White";
+            EndAction = false;
+        }
+
+        ~NXTControl()
+        {
+            if (PlierState)
+                ClosePliers();
+
+        }
+
+        /// <summary>
+        /// Fonction de connection au robot
+        /// </summary>
+        /// <returns>Message comme quoi le robot est connecté ou message d'erreur</returns>
+        public string ConnectRobot()
+        {
+            try
+            {
+                BlockMoteur.Connect();
+                return "Connecté";
+            }
+            catch (Exception e)
+            { return "Erreur de connection : " + e.Message; }
+            
+        }
+
+        /// <summary>
+        /// Fonction de mouvement du robot vers l'avant
+        /// </summary>
+        /// <param name="percentPower">Vitesse du moteur</param>
+        /// <param name="numberDegre">Degré d'avancement de la roue</param>
+        public void GoFront(int percentPower, uint numberDegre)
+        {
+            BlockMoteur.MotorB.Run((sbyte)percentPower, numberDegre);
+            BlockMoteur.MotorC.Run((sbyte)percentPower, numberDegre);
+
+        }
+
+        /// <summary>
+        /// Fait avancer le robot tant qu'on ne lui dit pas d'arrêter
+        /// </summary>
+        public void GoFrontWhileStop()
+        {
+            BlockMoteur.MotorB.Run((sbyte)20, 0);
+            BlockMoteur.MotorC.Run((sbyte)20, 0);
+        }
+
+        /// <summary>
+        /// Fonction de mouvement du robot vers la droite
+        /// </summary>
+        /// <param name="percentPower">Vitesse du moteur</param>
+        /// <param name="numberDegre">Degré d'avancement de la roue</param>
+        public void GoRight(int percentPower, uint numberDegre)
+        {
+            BlockMoteur.MotorC.Run((sbyte)percentPower, numberDegre);
+        }
+
+        /// <summary>
+        /// Fait tourner le robot a droite tant qu'on ne l'arrête pas
+        /// </summary>
+        public void GoRightWhileStop()
+        {
+            BlockMoteur.MotorC.Run((sbyte)50, 0);
+        }
+
+        /// <summary>
+        /// Fonction de mouvement du robot vers la gauche
+        /// </summary>
+        /// <param name="percentPower">Vitesse du moteur</param>
+        /// <param name="numberDegre">Degré d'avancement de la roue</param>
+        public void GoLeft(int percentPower, uint numberDegre)
+        {
+            BlockMoteur.MotorB.Run((sbyte)percentPower, numberDegre);
+        }
+
+        /// <summary>
+        /// Fait tourner le robot a gauche tant qu'on ne l'arrête pas
+        /// </summary>
+        public void GoLeftWhileStop()
+        {
+            BlockMoteur.MotorB.Run((sbyte)50, 0);
+        }
+
+        /// <summary>
+        /// Fonction de mouvement du robot vers l'arrière
+        /// </summary>
+        /// <param name="percentPower">Vitesse du moteur</param>
+        /// <param name="numberDegre">Degré d'avancement de la roue</param>
+        public void GoBack(int percentPower, uint numberDegre)
+        {
+            BlockMoteur.MotorB.Run((sbyte)-percentPower, numberDegre);
+            BlockMoteur.MotorC.Run((sbyte)-percentPower, numberDegre);
+
+        }
+
+        /// <summary>
+        /// Fait reculer le robot tant qu'on ne l'arrête pas
+        /// </summary>
+        public void GoBackWhileStop()
+        {
+            BlockMoteur.MotorB.Run((sbyte)-20, 0);
+            BlockMoteur.MotorC.Run((sbyte)-20, 0);
+        }
+
+        /// <summary>
+        /// Fonction de déconnection du robot
+        /// </summary>
+        /// <returns>Message comme quoi le robot est déconnecté ou message d'erreur</returns>
+        public string DisconnectRobot()
+        {
+            try
+            {
+                BlockMoteur.Disconnect();
+                return "Déconnecté";
+            }
+            catch (Exception e)
+            { return "Erreur de déconnection : " + e.Message; }
+
+        }
+
+        /// <summary>
+        /// Fonction de détection de la couleur présente sous le détecteur
+        /// </summary>
+        /// <returns>Retourne la couleur détectée</returns>
+        public string DectectColor()
+        {
+            string colorDectected = "Aucune couleur détectée";
+
+            try
+            {
+                CapteurCouleur.Poll();
+                colorDectected = CapteurCouleur.Color.Value.ToString();
+            }
+            catch (Exception e)
+            {
+                colorDectected = "Erreur capteur : " + e.Message;
+            }
+            
+
+            return colorDectected;
+        }
+
+        /// <summary>
+        /// Fonction qui arrête les moteurs du robot
+        /// </summary>
+        public void StopRobot()
+        {
+            BlockMoteur.MotorB.Idle();
+            BlockMoteur.MotorC.Idle();
+        }
+
+        /// <summary>
+        /// Changement d'état de l'action en cours
+        /// </summary>
+        public void ChangeEndAction()
+        {
+            if (EndAction)
+                EndAction = false;
+            else
+                EndAction = true;
+        }
+
+        /// <summary>
+        /// Fonction d'ouverture de la pince
+        /// </summary>
+        public string OpenPliers()
+        {
+            BlockMoteur.MotorA.Run(30, OuverturePince);
+            PlierState = true;
+            return "Ouverture Pince"; 
+        }
+
+        /// <summary>
+        /// Fonction de fermeture de la pince
+        /// </summary>
+        public string ClosePliers()
+        {
+            BlockMoteur.MotorA.Run(-30, OuverturePince);
+            PlierState = false;
+            return "Fermeture Pince";
+        }
+
+        /// <summary>
+        /// Fonction de détection de la couleur noire
+        /// </summary>
+        public void LookingForBlack(NxtPollable poll)
+        {
+            if (CapteurCouleur.Color.Value.ToString() == "Black")
+                ColorChecked = "Black";
+            else
+                ColorChecked = "Autre que noir";
+        }
+
+        public void ModeDectection()
+        {
+            BlockMoteur.Sensor2.PollInterval = 100;
+            BlockMoteur.Sensor2.OnPolled += new Polled(LookingForBlack);
+        }
+
+        public string GetColorChecked()
+        {
+            return ColorChecked;
+        }
+
+        public void FolowTheLine()
+        {
+
+        }
+    }
+}
---
+++ trunk/TestRobotJoseph/TestRobotWPF/MainWindow.xaml.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Threading;
+using System.Windows.Threading;
+
+namespace TestRobotWPF
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private NXTControl ControlRobot;
+        private DispatcherTimer TimerColor;
+        private bool EnterAutoMode;
+        public MainWindow()
+        {
+            InitializeComponent();
+            ControlRobot = new NXTControl();
+            this.grid.Focus();
+            EnterAutoMode = false;
+            listTransportedColor.Items.Add("Blue");
+            listTransportedColor.Items.Add("Black");
+            listTransportedColor.Items.Add("Yellow");
+            listTransportedColor.Items.Add("Green");
+            listTransportedColor.Items.Add("Unknown");
+        }
+
+        private void bt_disconnect_Click(object sender, RoutedEventArgs e)
+        {
+            string returnDisCon = ControlRobot.DisconnectRobot();
+            lb_con.Content = returnDisCon;
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            string returnCon = ControlRobot.ConnectRobot();
+            lb_con.Content = returnCon;
+        }
+
+        private void bt_goFront_Click(object sender, RoutedEventArgs e)
+        {
+            lb_state.Content = "Move Front";
+            ControlRobot.GoFront(75, 360);
+            lb_state.Content = "Stop";
+        }
+
+        private void bt_goBack_Click(object sender, RoutedEventArgs e)
+        {
+            lb_state.Content = "Move Back";
+            ControlRobot.GoBack(75, 360);
+            lb_state.Content = "Stop";
+        }
+
+        private void bt_goRight_Click(object sender, RoutedEventArgs e)
+        {
+            lb_state.Content = "Move Right";
+            ControlRobot.GoFrontRight();
+            lb_state.Content = "Stop";
+        }
+
+        private void bt_goLeft_Click(object sender, RoutedEventArgs e)
+        {
+            lb_state.Content = "Move Left";
+            ControlRobot.GoFrontLeft();
+            lb_state.Content = "Stop";
+        }
+
+        private void bt_colorDectect_Click(object sender, RoutedEventArgs e)
+        {
+            lb_colorDetected.Content = ControlRobot.DectectColor();
+        }
+
+        private void Grid_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.Z:
+                    lb_state.Content = "Move Front";
+                    ControlRobot.GoFrontWhileStop();
+                    break;
+                case Key.S:
+                    lb_state.Content = "Move Back";
+                    ControlRobot.GoBack(75, 360);
+                    break;
+                case Key.D:
+                    lb_state.Content = "Move Right";
+                    ControlRobot.GoRight();
+                    //ControlRobot.GoFrontRight();
+                    break;
+                case Key.Q:
+                    lb_state.Content = "Move Left";
+                    ControlRobot.GoLeft(75, 360);
+                    break;
+                case Key.E:
+                    lb_state.Content = ControlRobot.OpenPliers();
+                    break;
+                case Key.R:
+                    lb_state.Content = ControlRobot.ClosePliers();
+                    break;
+                case Key.Y:
+                    lb_state.Content = "Follow The Line!";
+
+                    if (EnterAutoMode)
+                        EnterAutoMode = false;
+                    else
+                    {
+                        EnterAutoMode = true;
+
+                        ControlRobot.ModeDectection();
+                        TimerColor = new DispatcherTimer();
+                        TimerColor.Interval = new TimeSpan(0, 0, 0, 0, 50);
+                        TimerColor.Tick += new EventHandler(AutomaticModeLine);
+                        TimerColor.Start();
+
+                        ControlRobot.SetAutomaticMode();
+                    }
+                        
+                    break;
+                case Key.T:
+                    ControlRobot.ModeDectection();
+                    TimerColor = new DispatcherTimer();
+                    TimerColor.Interval = new TimeSpan(0, 0, 0, 0, 100);
+                    TimerColor.Tick += new EventHandler(CheckColor);
+                    TimerColor.Start();
+                    //lb_colorDetected.Text = ControlRobot.GetColorChecked();
+                    break;
+            }
+        }
+
+        private void Grid_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key != Key.Y)
+            {
+                //Arrêt du robot
+                ControlRobot.StopRobot();
+                lb_state.Content = "Stop";
+            }
+            else
+            {
+                if (!EnterAutoMode)
+                {
+                    ControlRobot.SetAutomaticMode();
+                    //--------------TMP
+                    ControlRobot.StopRobot();
+                    //--------------
+                    lb_state.Content = "Stop";
+                    TimerColor.Stop();
+                }
+            }
+        }
+
+        private void CheckColor(object sender, EventArgs e)
+        {
+            lb_colorDetected.Content = ControlRobot.GetColorChecked();
+            lb_colorTransported.Content = ControlRobot.GetColorTransported();
+        }
+
+        private void AutomaticModeLine(object sender, EventArgs e)
+        {
+            lb_colorDetected.Content = ControlRobot.GetColorChecked();
+            lb_colorTransported.Content = ControlRobot.GetColorTransported();
+            ControlRobot.AutomateScript();
+        }
+
+        private void bt_wrongWay_Click(object sender, RoutedEventArgs e)
+        {
+            ControlRobot.GoWrongWay(1);
+            lb_state.Content = "Demi-tour droite";
+        }
+
+        private void bt_WrongWayLeft_Click(object sender, RoutedEventArgs e)
+        {
+            ControlRobot.GoWrongWay(0);
+            lb_state.Content = "Demi-tour gauche";
+        }
+
+        private void bt_open_Click(object sender, RoutedEventArgs e)
+        {
+            ControlRobot.OpenPliers();
+        }
+
+        private void bt_close_Click(object sender, RoutedEventArgs e)
+        {
+            ControlRobot.ClosePliers();
+        }
+
+        private void bt_sound_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void bt_setColor_Click(object sender, RoutedEventArgs e)
+        {
+            if (listTransportedColor.SelectedItem != null)
+            {
+                ControlRobot.SetColorTransported(listTransportedColor.SelectedValue.ToString());
+                lb_colorTransported.Content = listTransportedColor.SelectedValue.ToString();
+            }
+        }
+    }
+}
